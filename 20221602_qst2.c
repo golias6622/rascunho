166fdd5b6da604e8356d310b13320bb04e7f7283
Kayky Brenner <115883336+golias6622@users.noreply.github.com>
2023-02-16 15:18:39 -0300

    estr_dados qst2 16/02/2023

--- 20221602_qst2.c
+++ 20221602_qst2.c
@@ -1,6 +1,6 @@
 /*Escreva um programa que implementa uma struct Produto com os campos nome, pre¸co e tipo. O
 tipo deve ser armazenado como uma union que armazena um dos tipos: ALIMENTO, BEBIDA ou
-ELETRONICO. O programa deve ler os dados de um produto e imprimir os dados do produto.*/
+ELETRONICO. O programa deve ler os dados de um produto e imprimir os dados do produto..*/
 
 #include<stdio.h>
 #include<stdlib.h>
